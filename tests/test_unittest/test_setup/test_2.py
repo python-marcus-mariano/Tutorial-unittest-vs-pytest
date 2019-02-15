ca25f66b35d06aee0bbc8272fc1d1e3277feadb5
mmariano <marcusney@gmail.com>
2019-02-15 08:24:26 -0300

    Step 3: Test setup

---
+++ tests/test_unittest/test_setup/test_2.py
@@ -0,0 +1,31 @@
+"""
+Pytest versus Unittest
+
+Unitest versus Pytest em Português
+
+Homepage and documentation:
+https://github.com/python-marcus-mariano/Tutorial-unittest-vs-pytest
+
+
+Copyright (c) 2019, Marcus Mariano.
+License: MIT (see LICENSE for details)
+"""
+
+from tests.test_unittest.test_setup.base import SetupTestCase
+
+
+class Setup2TestCase(SetupTestCase):
+    """Tem que começar com orientação."""
+
+    def test_db_access_save(self):
+        """Test db saving."""
+        print('######### running save')
+    
+    def test_db_access_delete(self):
+        """Test db deletion."""
+        print('######### running delete')
+
+    def test_non_db_action(self):
+        """Test does not depend on DB."""
+        print('######### running non DB')
+
